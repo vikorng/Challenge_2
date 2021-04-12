b5fd7660e16e093f9345ac7fac5eb0b2da5a10cb
vikorng <80935824+vikorng@users.noreply.github.com>
2021-04-11 20:34:02 -0700

    modifying

--- Starter_Code_Challenge2/qualifier/app.py
+++ Starter_Code_Challenge2/qualifier/app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Loan Qualifier Application. test change testing
+"""Loan Qualifier Application. modify
 
 This is a command line application to match applicants with qualifying loans.
 
